d6065a8d1f22a99ff3811f437ae763364221b49a
emily <eap6787@gmail.com>
2022-05-15 17:41:41 -0600

    add delete post route

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -106,4 +106,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
+// delete post
+router.delete('/:id', (req, res) => {
+  Post.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((data) => {
+      if (!data) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
