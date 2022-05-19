57b85f68c9f8de8f0b2f26c102be0c68be6534b9
emily <eap6787@gmail.com>
2022-05-18 22:43:31 -0600

    add navbar functionality

---
+++ public/javascript/index.js
@@ -0,0 +1,34 @@
+const logoutEl = document.querySelector('#logout-el');
+const loginEl = document.querySelector('#login-el');
+const dashboardEl = document.querySelector('#dashboard-el');
+const homeEl = document.querySelector('#homepage-el');
+
+async function logout() {
+  const response = await fetch('/api/users/logout', {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    console.log('logged out');
+  } else {
+    alert(response.statusText);
+  }
+}
+
+function loginRedirect() {
+  window.location.replace('/login');
+}
+
+function dashboardRedirect() {
+  window.location.replace('/dashboard');
+}
+
+function homepageRedirect() {
+  window.location.replace('/homepage');
+}
+
+logoutEl.addEventListener('click', logout);
+loginEl.addEventListener('click', loginRedirect);
+dashboardEl.addEventListener('click', dashboardRedirect);
+homeEl.addEventListener('click', homepageRedirect);
